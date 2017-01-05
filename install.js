64f2e030053ff985e89c68a871c0ddb889e48a77
bergi <bergi@axolotlfarm.org>
2017-01-05 01:45:15 +0100

    initial commit

---
+++ install.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+require('shelljs/global')
+
+var program = require('commander')
+
+function clean () {
+  //rm('-rf', '.build')
+}
+
+function dist () {
+  mkdir('-p', '.build')
+
+  // clone bootstrap repo and copy it to custom
+  exec('git clone --depth 1 --branch v3-dev git@github.com:twbs/bootstrap.git .build/original')
+  cp('-r', '.build/original/*', '.build/custom/')
+
+  // apply changes to custom
+  cp('-r', 'custom/*', '.build/custom/')
+
+  // build distribution
+  cd('.build/custom')
+  exec('npm install')
+  exec('grunt dist')
+  cd('../..')
+
+  // copy distribution
+  cp('-r', '.build/custom/dist/*', 'dist/')
+  cp('-r', 'custom/dist/*', 'dist/')
+}
+
+program
+  .command('clean')
+  .action(clean)
+
+program
+  .command('dist')
+  .action(dist)
+
+program.parse(process.argv)
