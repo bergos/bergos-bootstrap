4100b5cd0a436a6a54f7d9c7dd5c3e2501ec0d03
bergi <bergi@axolotlfarm.org>
2017-01-12 02:41:45 +0100

    simplified universe, added footer class, added links to example, fixed install script

--- install.js
+++ install.js
@@ -5,14 +5,17 @@ require('shelljs/global')
 var program = require('commander')
 
 function clean () {
-  //rm('-rf', '.build')
+  rm('-rf', '.build')
 }
 
 function dist () {
   mkdir('-p', '.build')
 
-  // clone bootstrap repo and copy it to custom
+  // clone bootstrap repo
   exec('git clone --depth 1 --branch v3-dev git@github.com:twbs/bootstrap.git .build/original')
+
+  // copy everything to custom
+  mkdir('-p', '.build/custom/')
   cp('-r', '.build/original/*', '.build/custom/')
 
   // apply changes to custom
